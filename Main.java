194b95a03389d0fe5945ebb122470bcc8916e36e
Emilie-Karol-Seck <emiliekces@gmail.com>
2026-04-09 20:54:50 +0000

    MainV2

--- Main.java
+++ Main.java
@@ -2,16 +2,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Sudoku sudoku = SudokuLoader.chargerDepuisFichier("grille.txt");
+        Sudoku sudoku;
 
-        System.out.println("Grille initiale :");
+        try {
+            // 🔹 Si un fichier est donné en argument
+            if (args.length > 0) {
+                sudoku = SudokuLoader.chargerDepuisFichier(args[0]);
+            } else {
+                // 🔹 Sinon utiliser grille.txt par défaut
+                sudoku = SudokuLoader.chargerDepuisFichier("grille.txt");
+            }
+
+        } catch (Exception e) {
+            System.out.println(" Erreur lors du chargement du fichier !");
+            System.out.println("Vérifiez que le fichier existe et est bien formaté.");
+            return;
+        }
+
+        // 🔹 Affichage initial
+        System.out.println("\nGrille initiale :");
         SudokuPrinter.afficher(sudoku);
 
-        if (SudokuSolver.solve(sudoku)) {
+        // 🔹 Résolution
+        System.out.println("\nRésolution en cours...");
+        boolean solved = SudokuSolver.solve(sudoku);
+
+        // 🔹 Résultat
+        if (solved) {
             System.out.println("\nGrille résolue :");
             SudokuPrinter.afficher(sudoku);
         } else {
-            System.out.println("Aucune solution trouvée !");
+            System.out.println("\n Aucune solution trouvée !");
         }
     }
 }
